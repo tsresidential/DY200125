a1c7e97fe0a3f4520713830d855d427409f927ef
Olusola Ogunsola <Sundayshola48@gmail.com>
2025-01-19 22:19:58 -1100

    first first

---
+++ main.js
@@ -0,0 +1,14 @@
+ let user = "Olusola"
+
+ console.log(`My name is ${user}`) 
+
+ let num1 = 122
+ let num2 = 150
+
+ let sum = num1 + num2
+
+ console.log(sum)
+
+ function hello(){
+    console.log("hello world")
+ }
