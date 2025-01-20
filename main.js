eb72c7cae254e9cee979f7e31c5ef35ad9af34c3
Olusola Ogunsola <Sundayshola48@gmail.com>
2025-01-19 22:51:15 -1100

    first first

--- main.js
+++ main.js
@@ -9,6 +9,28 @@
 
  console.log(sum)
 
- function hello(){
-    console.log("hello world")
+ function add( a, b){
+    console.log(a + b)
  }
+
+ add(1, 2)
+
+ //write a function that takes three numbers and returns the largest.(if else statement is important here)
+
+
+function largestNumber(a,b,c){
+    if (a > b && a > c){
+        console.log(" A lagest")
+    }
+
+    else if ( b > a && b > c){
+        console.log("B is largest")
+    }
+
+    else{
+        console.log( " c is the greatest")
+    }
+
+}
+
+largestNumber(20000 ,3000,900)
